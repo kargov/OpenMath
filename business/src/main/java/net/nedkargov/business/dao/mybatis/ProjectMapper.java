8f7504bebc2885c78da13f81dbed507513cbd02d
NedyalkoKargov <kargov.nedyalko@gmail.com>
2015-01-26 00:27:53 +0100

    Projects CRUD

---
+++ business/src/main/java/net/nedkargov/business/dao/mybatis/ProjectMapper.java
@@ -0,0 +1,13 @@
+package net.nedkargov.business.dao.mybatis;
+
+import net.nedkargov.business.domain.ProjectDetailsInterface;
+import net.nedkargov.business.domain.ProjectInterface;
+
+import java.util.List;
+
+public interface ProjectMapper {
+    List<ProjectInterface> getProjects();
+    Integer insertProject(ProjectDetailsInterface project);
+    ProjectDetailsInterface getProjectDetails(Integer id);
+    void deleteProject(Integer id);
+}
